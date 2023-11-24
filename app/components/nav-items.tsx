7657fb418940c37eb8a09579ecb56135b5729f13
PabloChavez03 <pablooscarchavez@gmail.com>
2023-11-24 18:59:20 -0300

    add nav items & handler by outside block

--- app/components/nav-items.tsx
+++ app/components/nav-items.tsx
@@ -1,7 +1,49 @@
 'use client'
 
+import { PRODUCT_CATEGORIES } from "@/config"
+import { useOnClickOutside } from "@/hooks/use-on-click-outside"
+import { useEffect, useRef, useState } from "react"
+import { NavItem } from "./nav-item"
+
 export function NavItems () {
+  const [activeIndex, setActiveIndex] = useState<null | number>(null)
+
+  const isAnyOpen = activeIndex !== null
+
+  const navRef = useRef<HTMLDivElement | null>(null)
+
+  useEffect(() => {
+    const handler = (e: KeyboardEvent) => {
+      if (e.key === "Escape") {
+        setActiveIndex(null)
+      }
+    }
+
+    document.addEventListener("keydown", handler)
+    return () => document.removeEventListener("keydown", handler)
+  }, [])
+
+  useOnClickOutside(navRef, () => {
+    setActiveIndex(null)
+  })
+
   return (
-    <div className="flex gap-4 h-full"></div>
+    <div className="flex gap-4 h-full" ref={navRef}>
+      {PRODUCT_CATEGORIES.map((category, i) => {
+        const handleOpen = () => {
+          if (activeIndex === i) {
+            setActiveIndex(null)
+          } else {
+            setActiveIndex(i)
+          }
+        }
+
+        const isOpen = i === activeIndex
+
+        return (
+          <NavItem key={category.value} category={category} handleOpen={handleOpen} isOpen={isOpen} isAnyOpen={isAnyOpen} />
+        )
+      })}
+    </div>
   )
 }
