2f56598a509627180bcaa920a06b0fca6f4bc288
PabloChavez03 <pablooscarchavez@gmail.com>
2023-11-30 10:05:26 -0300

    add user account nav with dropdown

---
+++ app/components/user-account-nav.tsx
@@ -0,0 +1,40 @@
+'use client'
+
+import { Button } from "@/components/ui/button"
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuSeparator, DropdownMenuTrigger } from "@/components/ui/dropdown-menu"
+import { useAuth } from "@/hooks/use-auth"
+import { User } from "@/payload-types"
+import Link from "next/link"
+
+export function UserAccountNav ({ user }: {
+  user: User
+}) {
+  const {
+    signOut
+  } = useAuth()
+  return <DropdownMenu>
+    <DropdownMenuTrigger asChild className="overflow-visible">
+      <Button variant="ghost" size="sm" className="relative">My account</Button>
+    </DropdownMenuTrigger>
+
+    <DropdownMenuContent className="bg-white w-60" align="end">
+      <div className="flex items-center justify-start gap-2 p-2">
+        <div className="flex flex-col spay-y-0.5 leading-none">
+          <p className="font-medium text-sm text-black">{user.email}</p>
+        </div>
+      </div>
+
+      <DropdownMenuSeparator />
+
+      <DropdownMenuItem asChild>
+        <Link href="sell">
+          Seller Dashboard
+        </Link>
+      </DropdownMenuItem>
+
+      <DropdownMenuItem className="cursor-pointer" onClick={signOut}>
+        Log out
+      </DropdownMenuItem>
+    </DropdownMenuContent>
+  </DropdownMenu>
+}
